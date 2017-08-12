2b0ae21a8b6e17a2f793d5348c6b43ab4854fd60
BlessedKnight757 <30807783+BlessedKnight757@users.noreply.github.com>
2017-08-12 15:37:04 -0400

    Update main.cpp
    Tried to clear things up to setup how I want to try to work on this again. I plan to put more comments in here to further describe each bit of code I have. My main issues are the ordered list options and getting the variables to store inputs properly so the program can display the information as it was typed.

--- Inventory/main.cpp
+++ Inventory/main.cpp
@@ -1,5 +1,31 @@
 /*
+Author: Brandon Kennedy
 
+This is an old program that I got a decent grade on in class, 
+but I never got it working correctly. I want to get this program running
+the right way, so I can get better at coding. 
+
+The program should run as follows:
+1: Menu Prompt should ask the user what it would like to do. There are 
+seven different choices: 
+(1)Make a record of a new item
+(2)Make an alphabetic ordered list of the items made
+(3)Make an descending ordered list of the items made
+(4)Display the total quantity of all items that were made
+(5)Display the total value of all the items that were made
+(6)Display a list of insured items only
+(7)Ends the program
+
+2: Invalid entries such as char or strings should display an error message
+without causing the program to crash.
+
+3: If a new item has not been made, menu prompts 2, 3, and 6 should display
+a message that there was no items made.
+
+4: Each menu option should have its own block of code seperate from main.
+
+I am trying to avoid the use of global variables. I am trying to make the 
+most efficient code I can with what I am trying to accomplish.
 */
 
 #include <iostream>
@@ -18,23 +44,23 @@ struct items
 
 int main()
 {
-	int option1(int);
+	int option1(int);//Prototypes for the menu prompt options
 	int option2(int);
 	int option3(int);
 	int option4(int);
 	int option5(int);
 	int option6(int);
 	int option7(int);
-	int opt;
-	int answer = 0;//Menu Option variable
-    int i_count = 0;//Tracks the items recorded
-    int control;//Loop Control Variable
-    int stop;//Loop Control Variable - While Loops
-    int t_quantity = 0;//Tracks total Quantity
+	
+	int opt;//Menu Input Variable
+    	int i_count = 0;//Tracks the items recorded
+    	int control;//Loop Control Variable
+    	int stop;//Loop Control Variable - While Loops
+    	int t_quantity = 0;//Tracks total Quantity
 	float sum = 0.0;//Tracks total value of each item
-	items *item = new items[20];	//Holds the items created
+	items *item = new items[20];//Holds the items created
 
-	while(answer != 7)
+	while(opt != 7)
 	{
 		cout<<"Inventory Tracker\n"<<endl;
 		cout<<"1. New Item(s)"<<endl;
@@ -49,28 +75,20 @@ int main()
 
 		if(opt < 1 || opt > 7)//Input Validation
 		{
-        cout<<"Error:Invalid Entry!"<<endl;
-        cout<<"Please enter a valid option."<<endl;
-        cout<<"1. New Item(s)"<<endl;
-		cout<<"2. Total Items (Alpha)"<<endl;
-		cout<<"3. Total Items (Descending)"<<endl;
-		cout<<"4. Total Quantity"<<endl;
-		cout<<"5. Total Value"<<endl;
-		cout<<"6. Insured Items"<<endl;
-		cout<<"7. Quit"<<endl;
-		cout<<"______________________________"<<endl;
-		cin >>opt;
+        		cout<<"Error:Invalid Entry!"<<endl;
+        		cout<<"Please enter a valid option."<<endl;
+        		cout<<"1. New Item(s)"<<endl;
+			cout<<"2. Total Items (Alpha)"<<endl;
+			cout<<"3. Total Items (Descending)"<<endl;
+			cout<<"4. Total Quantity"<<endl;
+			cout<<"5. Total Value"<<endl;
+			cout<<"6. Insured Items"<<endl;
+			cout<<"7. Quit"<<endl;
+			cout<<"______________________________"<<endl;
+			cin>>opt;
 		}
 
-        if(opt == 1)answer=option1(opt);
-        else if(opt == 2)answer=option2(opt);
-        else if(opt == 3)answer=option3(opt);
-        else if(opt == 4)answer=option4(opt);
-        else if(opt == 5)answer=option5(opt);
-        else if(opt == 6)answer=option6(opt);
-        else answer=option7(opt);
-
-		if(answer == 1)
+		if(opt == 1)//Make New Item Record
 		{
 			cout<<"\nPlease enter item name: ";
 			cin>>item[i_count].name;
@@ -105,7 +123,7 @@ int main()
 			}
 			i_count++;//New item has been made
 		}
-		else if( answer == 2 )//Option 2 -couldn't figure out how to order this
+		else if(opt== 2)//Option 2 -couldn't figure out how to order this
 		{
 			string display = item[i_count].name;
 			if(i_count == 0)
@@ -115,22 +133,21 @@ int main()
 			for(control = 0; control < i_count; control++)
 			{
 				if(item[control].name < display)
-                {
-                    cout<<"\nItem name: "<<display<<endl;
-                    cout<<"\nPrice: $"<<item[i_count].price<<endl;
-                }
-                else
-				{
-				    cout<< "\nItem name: "<<item[control].name<< endl;
-                    cout<< "\nPrice: $"<<item[control].price << endl;
-				}
-
+                			{
+                    				cout<<"\nItem name: "<<display<<endl;
+                    				cout<<"\nPrice: $"<<item[i_count].price<<endl;
+                			}
+                		else
+					{
+		    				cout<< "\nItem name: "<<item[control].name<< endl;
+                    				cout<< "\nPrice: $"<<item[control].price << endl;
+					}
 			}
 		}
-		else if(answer == 3)//Option 3 - couldn't figure this one out either
-        {
-            float h_price = item[i_count].price;
-            if(i_count == 0)
+		else if(opt==3)//Option 3 - couldn't figure this one out either
+        	{
+            		float h_price = item[i_count].price;
+            		if(i_count == 0)
 			{
 				cout <<"List is empty!"<<endl;//Checks for empty list
 			}
@@ -138,33 +155,34 @@ int main()
 			for(control = 0; control < i_count; control++)
 			{
 				if(h_price > item[control].price)
-                {
-                cout << "\nItem name: "<<item[i_count].name<< endl;	//Displays item in list form
-				cout << "\nPrice: $"<<h_price<<endl;
-                }
-                else
-                {
-                    cout<<"\n\nItem name: "<<item[control].name<<endl;//
-                    cout<<"\nPrice: $"<<item[control].price<<endl;
-                }
-
-			}
-        }
-		else if(answer == 4)//Option 4
-			cout <<"Total quantity of item is: "<<t_quantity<<endl;//Total items quantity
-		else if(answer == 5)//Option 5
+                		{
+                			cout << "\nItem name: "<<item[i_count].name<< endl;	//Displays item in list form
+					cout << "\nPrice: $"<<h_price<<endl;
+               			}
+                		else
+                		{
+                    			cout<<"\n\nItem name: "<<item[control].name<<endl;//
+                    			cout<<"\nPrice: $"<<item[control].price<<endl;
+				}
+        	}
+			
+		else if(opt==4)//Option 4
+			cout<<"Total quantity of item is: "<<t_quantity<<endl;//Total items quantity
+		
+		else if(opt == 5)//Option 5
 		{
 			if(i_count == 0)
 			{
 				cout<<"List is empty!"<<endl;//Checks for an empty list
 			}
 			for(control = 0; control < i_count; control++)//Displays the total value of items
-                sum = sum + item[control].quantity * item[control].price;
+                	sum = sum + item[control].quantity * item[control].price;
+			
 			cout.setf(ios::fixed);
 			cout<<setprecision(2)<<endl;
 			cout<<"Total value of all asset is: $" <<sum<<'\n'<<endl;
 		}
-		else if(answer == 6)
+		else if(opt == 6)
 		{
 			if(i_count == 0)
 			{
@@ -180,42 +198,6 @@ int main()
 			}
 		}
 		else
-
-			return 0;
+		return 0;
 	}
-}
-
-int option1(int number1)
-{
-    return number1;
-}
-
-int option2(int number2)
-{
-    return number2;
-}
-
-int option3(int number3)
-{
-    return number3;
-}
-
-int option4(int number4)
-{
-    return number4;
-}
-
-int option5(int number5)
-{
-    return number5;
-}
-
-int option6(int number6)
-{
-    return number6;
-}
-
-int option7(int number7)
-{
-    return number7;
 }
